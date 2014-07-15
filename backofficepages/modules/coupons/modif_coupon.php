b44c7e626db4190fc129d1e732bc62d0f15cce33
ezzoubir <douiderezzoubir@gmail.com>
2014-07-15 16:19:59 +0000

    add copun files

--- backofficepages/modules/coupons/modif_coupon.php
+++ backofficepages/modules/coupons/modif_coupon.php
@@ -7,7 +7,7 @@ function ProposePhoto($UploadingFile)
         $handle = new upload($UploadingFile);
         if ($handle->uploaded) 
         {
-            $FileName='logo_marchand_'.time();
+            $FileName='logo_coupon_'.time();
             $Rep='../'.RepPhoto;
             $Rep2='../'.RepPhoto.'mins';
             $ext='.'.$handle->file_src_name_ext;
@@ -45,7 +45,7 @@ function ProposePhoto($UploadingFile)
         $handle = new upload($UploadingFile);
         if ($handle->uploaded) 
         {
-            $FileName='video_'.$_POST['marchand'].'_'.time();
+            $FileName='video_'.$_POST['titre'].'_'.time();
             $Rep='../'.RepPhoto;
             $Rep2='../'.RepPhoto.'mins';
             $ext='.'.$handle->file_src_name_ext;
@@ -65,7 +65,7 @@ function ProposePhoto($UploadingFile)
 	if(isset($_POST['supprimer_logo']))
        {
        // on teste si ancienne image 
-            $sql='select logo from marchands where id="'.$_POST['id'].'"';
+            $sql='select logo from coupons where id="'.$_POST['id'].'"';
             $res=mysql_query($sql);
             $row=mysql_fetch_array($res);
             if($row['logo']!='')
@@ -74,7 +74,7 @@ function ProposePhoto($UploadingFile)
                  @unlink('../'.RepPhoto.'mins/'.$row['logo']);
             }
        
-             $sql='update marchands set logo="" where id="'.$_POST['id'].'"';
+             $sql='update coupons set logo="" where id="'.$_POST['id'].'"';
             mysql_query($sql);
        }
 	if(isset($_POST['del_image']))
@@ -92,46 +92,30 @@ function ProposePhoto($UploadingFile)
 	if(isset($_POST['modifier']))
   {
   
-	$sql='update marchands set marchand="'.$_POST['marchand'].'",
+	$sql='update coupons set titre="'.$_POST['titre'].'",
 		presentation="'.$_POST['presentation'].'",
 		url="'.$_POST['url'].'",
-		responsable="'.$_POST['responsable'].'",
-		responsable_email="'.$_POST['responsable_email'].'",
-		responsable_mobile="'.$_POST['responsable_mobile'].'",
-		tel="'.$_POST['tel'].'",
-		email="'.$_POST['email'].'",
-		adresse="'.$_POST['adresse'].'",
-		message="'.$_POST['message'].'",
 		site="'.$_POST['site'].'",
 		reduction="'.$_POST['reduction'].'",
-		facebook="'.$_POST['facebook'].'",
-		twitter="'.$_POST['twitter'].'",
-		youtube="'.$_POST['youtube'].'",
-		pinterest="'.$_POST['pinterest'].'",
-		linkedin="'.$_POST['linkedin'].'",
 		id_ville="'.$_POST['ville'].'",
+		id_marchand="'.$_POST['marchand'].'",
 		id_cat="'.$_POST['cat'].'" where id="'.$_POST['id'].'"';
 		
 	mysql_query($sql);
 	
-	$uploadlogo=ProposePhoto($_FILES['logo']);
-	if($uploadlogo!=false)
-		{
-			  $sql='update marchands set logo="'.$uploadlogo.'" where id="'.$_POST['id'].'"';
-			  mysql_query($sql);
-	}
-	
-	$uploadfile=ProposeFichier($_FILES['fichier']);
+		
+	$uploadlogo=ProposeFichier($_FILES['logo']);
 	if($uploadfile!=false)
 		{
-			  $sql='update marchands set fichier="'.$uploadfile.'" where id="'.$_POST['id'].'"';
+			  $sql='update coupons set logo="'.$uploadfile.'" where id="'.$_POST['id'].'"';
 			  mysql_query($sql);
 	}
 	
+		
 	$upload=ProposePhoto($_FILES['photo']);
     if($upload!=false)
     {
-          $sql='insert into photos (id_marchand,photo) values("'.$_POST['id'].'","'.$upload.'")';
+          $sql='insert into photos (id_coupon,photo) values("'.$_POST['id'].'","'.$upload.'")';
           mysql_query($sql);
     
     }
@@ -141,14 +125,14 @@ function ProposePhoto($UploadingFile)
 	$msg='<script>alert("Les données ont été modifié avec succès")</script>';
   }
   
-  $sd='select * from marchands where id="'.$_GET['id'].'"';
+  $sd='select * from coupons where id="'.$_GET['id'].'"';
   $fg=mysql_query($sd);
   $dr=mysql_fetch_assoc($fg);
 ?>
-<h2>Ajouter une marchand</h2>
+<h2>Ajouter une coupon</h2>
 <form action="<?php echo $_SERVER['REQUEST_URI']; ?>" method="post" enctype="multipart/form-data">
 <table width="100%" cellspacing="5" cellpadding="0">
-		<tr><td>marchand</td><td><input type="text" name="marchand" value="<?php echo $dr['marchand']; ?>" /></td></tr>
+		<tr><td>Titre</td><td><input type="text" name="titre" value="<?php echo $dr['titre']; ?>" /></td></tr>
 		<tr><td>Logo</td><td>
 		<?php if($dr['logo']!='') { ?>
 		<a href="<?php echo BASE_URL.RepPhoto.$dr['logo']; ?>"><img src="../images/photos/<?php echo $dr['logo']; ?>" height="110" width="200" style="border:2px solid #ccc;" /></a>
@@ -157,17 +141,9 @@ function ProposePhoto($UploadingFile)
 		<input type="file" name="logo" />
 		<?php } ?></td></tr>
 		<tr><td>Présentation</td><td><textarea cols="15" rows="3" name="presentation"><?php echo $dr['presentation']; ?></textarea></td></tr>
-		<tr><td>Video (fichier)</td><td><input type="file" name="fichier" value="" /></td></tr>
 		<tr><td>Video (url)</td><td><input type="text" name="url" value="<?php echo $dr['url']; ?>" /></td></tr>
-		<tr><td>Responsable</td><td><input type="text" name="responsable" value="<?php echo $dr['responsable']; ?>" /></td></tr>
-		<tr><td>Email (Responsable)</td><td><input type="text" name="responsable_email" value="<?php echo $dr['responsable_email']; ?>" /></td></tr>
-		<tr><td>Mobile (Responsable)</td><td><input type="text" name="responsable_mobile" value="<?php echo $dr['responsable_mobile']; ?>" /></td></tr>
-		<tr><td>Tél</td><td><input type="text" name="tel" value="<?php echo $dr['tel']; ?>" /></td></tr>
-		<tr><td>email</td><td><input type="text" name="email" value="<?php echo $dr['email']; ?>" /></td></tr>
-		<tr><td>Adresse</td><td><input type="text" name="adresse" value="<?php echo $dr['adresse']; ?>" /></td></tr>
 		<tr><td>Web site</td><td><input type="text" name="site" value="<?php echo $dr['site']; ?>" /></td></tr>
 		<tr><td>Réduction</td><td><input type="text" name="reduction" value="<?php echo $dr['reduction']; ?>" /></td></tr>
-		<tr><td>Map</td><td><textarea name="message"><?php echo $dr['message']; ?></textarea></td></tr>
 		<tr><td>Ville</td><td><select name="ville">
 		<option value=""></option>
 		<?php
@@ -178,7 +154,17 @@ function ProposePhoto($UploadingFile)
 		<option value="<?php echo $dt['id']; ?>" <?php if($dr['id_ville']==$dt['id']) { echo 'selected';} ?>><?php echo $dt['ville']; ?></option>
 		<?php } ?>
 		</select></td></tr>
-		<tr><td>cat</td><td><select name="cat">
+		<tr><td>Marchand</td><td><select name="marchand">
+		<option value=""></option>
+		<?php
+			$sq='select * from marchands order by marchand asc';
+			$rq=mysql_query($sq);
+			while($dt=mysql_fetch_array($rq)){
+		?>
+		<option value="<?php echo $dt['id']; ?>" <?php if($dr['id_marchand']==$dt['id']) { echo 'selected';} ?>><?php echo $dt['marchand']; ?></option>
+		<?php } ?>
+		</select></td></tr>
+		<tr><td>Catégorie</td><td><select name="cat">
 		<option value=""></option>
 		<?php
 			$sq='select * from cats order by cat asc';
@@ -194,7 +180,7 @@ function ProposePhoto($UploadingFile)
         <br/>
         <div style=" margin-top:25px;">
         <?php
-          $sql='select * from photos where id_marchand="'.(int)$_GET['id'].'" order by id_photo';
+          $sql='select * from photos where id_coupon="'.(int)$_GET['id'].'" order by id_photo';
           $res=mysql_query($sql);
           while($row=mysql_fetch_array($res))
           {
@@ -212,11 +198,6 @@ function ProposePhoto($UploadingFile)
         </div>
 			</td>
 		</tr>
-		<tr><td>Facebook</td><td><input type="text" name="facebook" value="<?php echo $dr['facebook']; ?>" /></td></tr>
-		<tr><td>Twitter</td><td><input type="text" name="twitter" value="<?php echo $dr['twitter']; ?>" /></td></tr>
-		<tr><td>Youtube</td><td><input type="text" name="youtube" value="<?php echo $dr['youtube']; ?>" /></td></tr>
-		<tr><td>Pinterest</td><td><input type="text" name="pinterest" value="<?php echo $dr['pinterest']; ?>" /></td></tr>
-		<tr><td>Linkedin</td><td><input type="text" name="linkedin" value="<?php echo $dr['linkedin']; ?>" /></td></tr>
 		<tr><td></td><td><input type="submit" name="modifier" value="Modifier" /></td></tr>
 </table>
 <input type="hidden" name="id" value="<?php echo $_GET['id']; ?>" />
